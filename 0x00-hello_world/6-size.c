1a464b31865a268a8174d8fe6b20ad0620347782
possessedcodes <soalawilliams0212@gmail.com>
2022-09-12 04:43:54 -0700

    Finally

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main (void)
+{
+	char c;
+	int i;
+	long li;
+	long long lii;
+	float f;
+
+	printf("size of a char: %ld byte(s)/n", sizeof(c));
+	Printf("size of an int: %ld byte(s)\n", sizeof(i));
+	Printf("size of a long i: %ld byte(s)\n", sizeof(li));
+	Printf("size of a long long i: %ld byte(s)\n", sizeof(lli));
+	Printf("size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
